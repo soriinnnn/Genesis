5b98cb5f65aca0fbd22cd5b289d944bba871d30a
Sorin Chiperi Arnaut <sorinchiperiarnaut@gmail.com>
2026-07-10 15:44:28 +0200

    Afegit un missatge d'informació sobre el motor de física.

--- source/physics/PhysicsEngine.cpp
+++ source/physics/PhysicsEngine.cpp
@@ -66,6 +66,8 @@ PhysicsEngine::PhysicsEngine(const PhysicsEngineDesc& desc): Base(desc.base)
 	m_drawSettings.mDrawShape = true;
 	m_drawSettings.mDrawBoundingBox = false;
 #endif
+
+	GENESIS_LOG_INFO("PhysicsEngine initialized with a maximum of {} bodies.", MAX_BODIES);
 }
 
 PhysicsEngine::~PhysicsEngine() 
